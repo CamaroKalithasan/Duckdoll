21ed4f18a89facc94c52ca0ea762cd06148ca324
Camaro Kalithasan <CCKalithasan@student.fullsail.edu>
2023-06-27 16:35:49 -0400

    Duck Character added
    #ADD: NEW Ducky character with animations and rigging
    #EDIT: Main Menu character updated with new character and jump animations
    #TO-DO: Game scene character needs to be updated with new character

--- Assets/Gameplay/MainMenuCharacter.cs
+++ Assets/Gameplay/MainMenuCharacter.cs
@@ -7,19 +7,31 @@ public class MainMenuCharacter : MonoBehaviour
     Rigidbody rb;
     private bool isJumping;
     public AudioSource audioSource;
+    public Animator animator;
+    public string idleAnimationName = "Idle_A";
+    public string jumpAnimationName = "Fly";
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+        
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("space") && !isJumping)
+        if (!isJumping)
+        {
+            animator.Play(idleAnimationName);
+        }
+
+        if ((Input.GetKey("space") || Input.GetKey("joystick button 0")) && !isJumping)
         {
             rb.velocity = new Vector3(0, 5f, 0);
             isJumping = true;
+            animator.Play(jumpAnimationName);
             audioSource.Play();
         }
     }
