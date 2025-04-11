29f48854d9dd80f2ba0f65af1900e1000f03aeba
CamaroKalithasan <CCKalithasan@student.fullsail.edu>
2025-04-10 22:19:39 -0400

    FULL CLEAN
    #EDIT:
    - Nuked ENTIRE project
    - Created new Unreal project of Duckdoll with c++ added
    - Completely fresh start, solution has been built, project files have been regenerated and levels were built inside unreal
    - Added gitignore

---
+++ Source/Duckdoll/DuckdollGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DuckdollGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADuckdollGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADuckdollGameMode();
+};
+
+
+
